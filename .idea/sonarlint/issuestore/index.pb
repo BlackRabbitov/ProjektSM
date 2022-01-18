1c9c3802dbcf9d197fbca60d53907963eede4788
Michal <michal332111@o2.pl>
2022-01-18 02:57:15 +0100

    Navigation Menu
    -navigation menu interface connected with main menu.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,5 +3,11 @@ l
 <app/src/main/java/com/michal/projektsm/RegisterActivity.java,e\1\e1429362d111bfa8e3183e59e1672dac8a445856
 u
 Eapp/src/main/java/com/michal/projektsm/roomdatabase/UserDatabase.java,0\a\0a95c906af540799f6283074ac4c15f83f8f7e16
+k
+;app/src/main/java/com/michal/projektsm/BorrowerAdapter.java,b\7\b73ebeadfa9529304e2054c1303c8ba48512d8c2
+l
+<app/src/main/java/com/michal/projektsm/BorrowerActivity.java,a\a\aaaa582301951d0778548ed0e90648bf7aadfbd8
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
 h
 8app/src/main/java/com/michal/projektsm/MainActivity.java,0\e\0e53dc190fee57320c3eefa504564353362c0b21
--- app/src/main/java/com/michal/projektsm/MainActivity.java
+++ app/src/main/java/com/michal/projektsm/MainActivity.java
@@ -1,15 +1,20 @@
 package com.michal.projektsm;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView tName;
 
@@ -24,8 +29,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //navigation
-        //drawerLayout = findViewById(R.id.drawer_layout);
-        //navigationView = findViewById(R.id.nav_view);
-        //drawerLayout = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar3);
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+        //navigation menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
