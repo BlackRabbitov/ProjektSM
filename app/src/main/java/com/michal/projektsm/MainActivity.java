13c8ab4441510d7100b8b612080c520316601c09
Michal <michal332111@o2.pl>
2022-01-18 01:53:34 +0100

    Main Menu
    -main menu interface

--- app/src/main/java/com/michal/projektsm/MainActivity.java
+++ app/src/main/java/com/michal/projektsm/MainActivity.java
@@ -1,21 +1,31 @@
 package com.michal.projektsm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tName;
+
+    //navigation view variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tName = findViewById(R.id.name);
-        String name = getIntent().getStringExtra("name");
-        tName.setText(name);
+        //navigation
+        //drawerLayout = findViewById(R.id.drawer_layout);
+        //navigationView = findViewById(R.id.nav_view);
+        //drawerLayout = findViewById(R.id.toolbar);
     }
 }
