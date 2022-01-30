802f25fcb33f24ae84fba1e6680e0603a321e24a
Michal <michal332111@o2.pl>
2022-01-30 22:04:41 +0100

    Upgrades for currency and fixes

--- app/src/main/java/com/michal/projektsm/CurrencyActivity.java
+++ app/src/main/java/com/michal/projektsm/CurrencyActivity.java
@@ -82,15 +82,19 @@ public class CurrencyActivity extends AppCompatActivity {
             CurrencyConverter currencyConverter = CurrencyConverter.getInstance();
             currencyConverter.setCurrencyName("PLN");
             currencyConverter.setMultiplier(1.0f);
+            Toast.makeText(CurrencyActivity.this, getApplicationContext().getString(R.string.pln_change), Toast.LENGTH_SHORT).show();
         });
         findViewById(R.id.btnEUR).setOnClickListener(v -> {
             this.APIFunction("EUR");
+            Toast.makeText(CurrencyActivity.this, getApplicationContext().getString(R.string.eur_change), Toast.LENGTH_SHORT).show();
         });
         findViewById(R.id.btnUSD).setOnClickListener(v -> {
             this.APIFunction("USD");
+            Toast.makeText(CurrencyActivity.this, getApplicationContext().getString(R.string.usd_change), Toast.LENGTH_SHORT).show();
         });
         findViewById(R.id.btnGBP).setOnClickListener(v -> {
             this.APIFunction("GBP");
+            Toast.makeText(CurrencyActivity.this, getApplicationContext().getString(R.string.gbp_change), Toast.LENGTH_SHORT).show();
         });
     }
 
